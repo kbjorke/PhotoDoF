90b8b66b58331695b8c20ea4818cac9f81b19557
Kristian Bjoerke <kristian.bjoerke@cern.ch>
2019-07-11 12:08:17 +0200

    Prepared for all plots to be made, need to decide on final ranges.

--- dofcalc.py
+++ dofcalc.py
@@ -54,6 +54,17 @@ if __name__=="__main__":
     F_array = np.array([1.4, 2, 2.8, 4, 5.6, 8, 11, 16, 22])
     dist_array = np.array([0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4])
     focl_array = np.array([16, 20, 30, 40, 50, 100, 210])
+    
+    dist_array2D = np.array([
+        [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
+        [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
+        [0.5, 1, 1.5, 2, 3, 5, 8, 15],
+        [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
+        [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
+        [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
+        [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4]
+        ]
+        )
 
     nF = len(F_array)
     ndist = len(dist_array)
@@ -109,93 +120,96 @@ if __name__=="__main__":
 
     canvas1.SaveAs("hyperfocal-distance.eps")
 
-    focl = 30
+    #focl = 30
+    #l = 2
+    for l in range(nfocl):
+        focl = focl_array[l]
 
-    h_dof = TH2D("hist_dof", "hist_dof", ndist, 0, ndist, nF, 0, nF) 
-    h_fnrat = TH2D("hist_fnrat", "hist_fnrat", ndist, 0, ndist, nF, 0, nF) 
+        h_dof = TH2D("hist_dof_focl%d" %focl, "hist_dof_focl%d" %focl, ndist, 0, ndist, nF, 0, nF) 
+        h_fnrat = TH2D("hist_fnrat_focl%d" %focl, "hist_fnrat_focl%d" %focl, ndist, 0, ndist, nF, 0, nF) 
 
-    for i in range(ndist):
-        dist = dist_array[i]
+        for i in range(ndist):
+            dist = dist_array2D[l][i]
+            for j in range(nF):
+                F = F_array[j]
+                dof_ = dof(focl,F,dist)
+                if dof_ > 0:
+                    h_dof.SetBinContent(i+1,nF-j,dof_)
+                    h_fnrat.SetBinContent(i+1,nF-j,dof2(focl,F,dist)/dof1(focl,F,dist))
+
+        for i in range(ndist):
+            h_dof.GetXaxis().SetBinLabel(i+1, "%3.1f m" % dist_array2D[l][i])
+            h_fnrat.GetXaxis().SetBinLabel(i+1, "%3.1f m" % dist_array2D[l][i])
         for j in range(nF):
-            F = F_array[j]
-            dof_ = dof(focl,F,dist)
-            if dof_ > 0:
-                h_dof.SetBinContent(i+1,nF-j,dof_)
-                h_fnrat.SetBinContent(i+1,nF-j,dof2(focl,F,dist)/dof1(focl,F,dist))
+            h_dof.GetYaxis().SetBinLabel(nF-j, "f/%3.1f" % F_array[j])
+            h_fnrat.GetYaxis().SetBinLabel(nF-j, "f/%3.1f" % F_array[j])
 
-    for i in range(ndist):
-        h_dof.GetXaxis().SetBinLabel(i+1, "%3.1f m" % dist_array[i])
-        h_fnrat.GetXaxis().SetBinLabel(i+1, "%3.1f m" % dist_array[i])
-    for j in range(nF):
-        h_dof.GetYaxis().SetBinLabel(nF-j, "f/%3.1f" % F_array[j])
-        h_fnrat.GetYaxis().SetBinLabel(nF-j, "f/%3.1f" % F_array[j])
+        canvas2 = TCanvas("canvas2", "canvas2", 1500, 900)
+        canvas2.SetLogz()
+        canvas2.SetGrid()
+        canvas2.SetLeftMargin(0.10)
+        canvas2.SetRightMargin(0.16)
+        canvas2.SetTopMargin(0.11)
+        canvas2.SetBottomMargin(0.10)
+        gStyle.SetOptStat(0)
+        gStyle.SetPaintTextFormat(".2f m")
+        gStyle.SetTextFont(40)
+        h_dof.SetTitle("")
+        h_dof.GetXaxis().SetTitle("Focus length")
+        h_dof.GetXaxis().CenterTitle()
+        h_dof.GetXaxis().SetTitleSize(0.05)
+        h_dof.GetXaxis().SetTitleOffset(0.9)
+        h_dof.GetXaxis().SetLabelSize(0.05)
+        h_dof.GetYaxis().SetTitle("Aperture")
+        h_dof.GetYaxis().SetTitleSize(0.05)
+        h_dof.GetYaxis().SetTitleOffset(0.9)
+        h_dof.GetYaxis().SetLabelSize(0.05)
+        h_dof.GetYaxis().CenterTitle()
+        h_dof.GetZaxis().SetRangeUser(0.01, 30.0)
+        h_dof.GetZaxis().SetNoExponent()
+        h_dof.GetZaxis().SetTitle("Depth of field [m]")
+        h_dof.GetZaxis().SetTitleSize(0.05)
+        h_dof.GetZaxis().SetTitleOffset(0.9)
+        h_dof.GetZaxis().CenterTitle()
+        h_dof.SetMarkerSize(1.4)
+        h_dof.Draw("COLZ X+")
+        h_dof.Draw("TEXT same")
+        
+        canvas2.SaveAs("depth-of-field_focl%d.eps" %focl)
 
-    canvas2 = TCanvas("canvas2", "canvas2", 1500, 900)
-    canvas2.SetLogz()
-    canvas2.SetGrid()
-    canvas2.SetLeftMargin(0.10)
-    canvas2.SetRightMargin(0.16)
-    canvas2.SetTopMargin(0.11)
-    canvas2.SetBottomMargin(0.10)
-    gStyle.SetOptStat(0)
-    gStyle.SetPaintTextFormat(".2f m")
-    gStyle.SetTextFont(40)
-    h_dof.SetTitle("")
-    h_dof.GetXaxis().SetTitle("Focus length")
-    h_dof.GetXaxis().CenterTitle()
-    h_dof.GetXaxis().SetTitleSize(0.05)
-    h_dof.GetXaxis().SetTitleOffset(0.9)
-    h_dof.GetXaxis().SetLabelSize(0.05)
-    h_dof.GetYaxis().SetTitle("Aperture")
-    h_dof.GetYaxis().SetTitleSize(0.05)
-    h_dof.GetYaxis().SetTitleOffset(0.9)
-    h_dof.GetYaxis().SetLabelSize(0.05)
-    h_dof.GetYaxis().CenterTitle()
-    h_dof.GetZaxis().SetRangeUser(0.01, 30.0)
-    h_dof.GetZaxis().SetNoExponent()
-    h_dof.GetZaxis().SetTitle("Depth of field [m]")
-    h_dof.GetZaxis().SetTitleSize(0.05)
-    h_dof.GetZaxis().SetTitleOffset(0.9)
-    h_dof.GetZaxis().CenterTitle()
-    h_dof.SetMarkerSize(1.4)
-    h_dof.Draw("COLZ X+")
-    h_dof.Draw("TEXT same")
-    
-    canvas2.SaveAs("depth-of-field.eps")
+        canvas3 = TCanvas("canvas3", "canvas3", 1500, 900)
+        canvas3.SetLogz()
+        canvas3.SetGrid()
+        canvas3.SetLeftMargin(0.10)
+        canvas3.SetRightMargin(0.16)
+        canvas3.SetTopMargin(0.11)
+        canvas3.SetBottomMargin(0.10)
+        gStyle.SetOptStat(0)
+        gStyle.SetPaintTextFormat(".2f")
+        gStyle.SetTextFont(40)
+        h_fnrat.SetTitle("")
+        h_fnrat.GetXaxis().SetTitle("Focus length")
+        h_fnrat.GetXaxis().CenterTitle()
+        h_fnrat.GetXaxis().SetTitleSize(0.05)
+        h_fnrat.GetXaxis().SetTitleOffset(0.9)
+        h_fnrat.GetXaxis().SetLabelSize(0.05)
+        h_fnrat.GetYaxis().SetTitle("Aperture")
+        h_fnrat.GetYaxis().SetTitleSize(0.05)
+        h_fnrat.GetYaxis().SetTitleOffset(0.9)
+        h_fnrat.GetYaxis().SetLabelSize(0.05)
+        h_fnrat.GetYaxis().CenterTitle()
+        h_fnrat.GetZaxis().SetRangeUser(1.00, 5.00)
+        h_fnrat.GetZaxis().SetNoExponent()
+        h_fnrat.GetZaxis().SetMoreLogLabels()
+        h_fnrat.GetZaxis().SetTitle("Far-near ratio")
+        h_fnrat.GetZaxis().SetTitleSize(0.05)
+        h_fnrat.GetZaxis().SetTitleOffset(0.7)
+        h_fnrat.GetZaxis().CenterTitle()
+        h_fnrat.SetMarkerSize(1.4)
+        h_fnrat.Draw("COLZ X+")
+        h_fnrat.Draw("TEXT same")
+        
+        canvas3.SaveAs("far-near-ratio_focl%d.eps" %focl)
 
-    canvas3 = TCanvas("canvas3", "canvas3", 1500, 900)
-    canvas3.SetLogz()
-    canvas3.SetGrid()
-    canvas3.SetLeftMargin(0.10)
-    canvas3.SetRightMargin(0.16)
-    canvas3.SetTopMargin(0.11)
-    canvas3.SetBottomMargin(0.10)
-    gStyle.SetOptStat(0)
-    gStyle.SetPaintTextFormat(".2f")
-    gStyle.SetTextFont(40)
-    h_fnrat.SetTitle("")
-    h_fnrat.GetXaxis().SetTitle("Focus length")
-    h_fnrat.GetXaxis().CenterTitle()
-    h_fnrat.GetXaxis().SetTitleSize(0.05)
-    h_fnrat.GetXaxis().SetTitleOffset(0.9)
-    h_fnrat.GetXaxis().SetLabelSize(0.05)
-    h_fnrat.GetYaxis().SetTitle("Aperture")
-    h_fnrat.GetYaxis().SetTitleSize(0.05)
-    h_fnrat.GetYaxis().SetTitleOffset(0.9)
-    h_fnrat.GetYaxis().SetLabelSize(0.05)
-    h_fnrat.GetYaxis().CenterTitle()
-    h_fnrat.GetZaxis().SetRangeUser(1.00, 5.00)
-    h_fnrat.GetZaxis().SetNoExponent()
-    h_fnrat.GetZaxis().SetMoreLogLabels()
-    h_fnrat.GetZaxis().SetTitle("Far-near ratio")
-    h_fnrat.GetZaxis().SetTitleSize(0.05)
-    h_fnrat.GetZaxis().SetTitleOffset(0.7)
-    h_fnrat.GetZaxis().CenterTitle()
-    h_fnrat.SetMarkerSize(1.4)
-    h_fnrat.Draw("COLZ X+")
-    h_fnrat.Draw("TEXT same")
-    
-    canvas3.SaveAs("far-near-ratio.eps")
-
-    raw_input("")
+        #raw_input("")
 
